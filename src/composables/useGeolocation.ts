eff381cf645d827b1c8975031847720ab4d8e6e5
Aleksan <sanyok.10rus@gmail.com>
2025-08-01 23:39:48 +0300

    changed composable

--- src/composables/useGeolocation.ts
+++ src/composables/useGeolocation.ts
@@ -1,10 +1,13 @@
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
+
+const URL = 'https://nominatim.openstreetmap.org/reverse'
 
 export const useGeolocation = () => {
   const latitude = ref<number | null>(null)
   const longitude = ref<number | null>(null)
+  const loading = ref<boolean>(true)
   const error = ref<string | null>(null)
-
+  const city = ref<string | null>(null)
   const getLocation = () => {
     if (!navigator.geolocation) {
       error.value = 'Browser is not supports geolocation API'
@@ -22,9 +25,24 @@ export const useGeolocation = () => {
     )
   }
 
-  onMounted(() => {
-    getLocation()
-  })
+  const getCity = async (URL: string, latitude: number, longitude: number) => {
+    try {
+      getLocation()
+      const res = await fetch(`${URL}?lat=${latitude}&lon=${longitude}&format=json`, {
+        headers: {
+          'User-Agent': 'MyVueApp/1.0',
+        },
+      })
+
+      const data = await res.json()
+      city.value = data.address.city || data.address.town || data.address.village || 'Неизвестно'
+    } catch (err) {
+      error.value = err.message | 'Something wents wrong'
+    } finally {
+      loading.value = false
+    }
+  }
+
   return {
     latitude,
     longitude,
